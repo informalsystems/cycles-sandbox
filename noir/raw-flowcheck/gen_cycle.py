7481a8bf5d4d3e3b8a8a1b9603dc41c0875b8132
Andrew Miller <socrates1024@gmail.com>
2024-03-25 17:20:25 -0500

    added py file for genearting a cyclic flow

---
+++ noir/raw-flowcheck/gen_cycle.py
@@ -0,0 +1,35 @@
+import random
+from collections import defaultdict
+
+V = 16
+E = 32
+
+edges = defaultdict(lambda: 0)
+while len(edges) < E:
+    # Length of cycle
+    n = random.randint(1,min(5,E-len(edges)))
+
+    nodes = [random.randint(0,V-1) for _ in range(n)]
+
+    # Amount
+    amt = random.randint(1,100)
+    
+    for (src,dst) in zip(nodes, nodes[1:]+nodes[:1]):
+        edges[(src,dst)] += amt
+
+edges = [[src,dst,amt] for (src,dst),amt in edges.items() if src != dst]
+edges += [[0,0,0]]*(E-len(edges))
+
+def kerckhoff(edges):
+    v = defaultdict(lambda: 0)
+    for (src,dst,amt) in edges:
+        v[src] -= amt
+        v[dst] += amt
+    for val in v.values():
+        assert(val == 0)
+    print('ok')
+kerckhoff(edges)
+
+print(edges)
+#for e in edges:
+#    print(list(e))
