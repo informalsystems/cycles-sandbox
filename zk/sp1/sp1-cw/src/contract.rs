7343f586a30df3d01a6b514e715d8561bc756644
hu55a1n1 <sufialhussaini@gmail.com>
2024-10-24 23:52:42 +0200

    Add verifier code

--- zk/sp1/sp1-cw/src/contract.rs
+++ zk/sp1/sp1-cw/src/contract.rs
@@ -1,7 +1,8 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult};
 // use cw2::set_contract_version;
+use sp1_sdk::{ProverClient, SP1ProofWithPublicValues, SP1VerifyingKey};
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
@@ -27,9 +28,33 @@ pub fn execute(
     _deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    _msg: ExecuteMsg,
+    msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    match msg {
+        ExecuteMsg::VerifyProof {
+            proof_bytes,
+            verifying_key_bytes,
+        } => {
+            let client = ProverClient::new();
+
+            let proof: SP1ProofWithPublicValues =
+                bincode::deserialize(&proof_bytes).map_err(|e| {
+                    ContractError::Std(StdError::parse_err(
+                        "SP1ProofWithPublicValues",
+                        e.to_string(),
+                    ))
+                })?;
+
+            let vk: SP1VerifyingKey = bincode::deserialize(&verifying_key_bytes).map_err(|e| {
+                ContractError::Std(StdError::parse_err("SP1VerifyingKey", e.to_string()))
+            })?;
+
+            client
+                .verify(&proof, &vk)
+                .map(|_| Response::new())
+                .map_err(|e| ContractError::Std(StdError::generic_err(e.to_string())))
+        }
+    }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
--- zk/sp1/sp1-cw/src/msg.rs
+++ zk/sp1/sp1-cw/src/msg.rs
@@ -1,10 +1,16 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::HexBinary;
 
 #[cw_serde]
 pub struct InstantiateMsg {}
 
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    VerifyProof {
+        proof_bytes: HexBinary,
+        verifying_key_bytes: HexBinary,
+    },
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
