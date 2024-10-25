f62555fa382d3cafc7d641422df419c23f2599b4
hu55a1n1 <sufialhussaini@gmail.com>
2024-10-25 14:58:42 +0200

    Major cleanup for reducing bin size

--- zk/sp1/sp1-cw/src/contract.rs
+++ zk/sp1/sp1-cw/src/contract.rs
@@ -1,17 +1,10 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult};
-// use cw2::set_contract_version;
 use sp1_sdk::{ProverClient, SP1ProofWithPublicValues, SP1VerifyingKey};
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-
-/*
-// version info for migration info
-const CONTRACT_NAME: &str = "crates.io:sp1-cw";
-const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-*/
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
