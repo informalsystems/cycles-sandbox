8c8927ccdaecf944eeb9752ea312b7bf2619967d
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-01-23 16:14:04 +0400

    Param gen for settlement circuit

---
+++ zk/arkworks/gramine/gen/settlement_id.rs
@@ -0,0 +1,3 @@
+
+pub const PROVING_KEY_ID: &'static str = "groth16pk1uh9u8e4y2n3fe0rgc4t7ravqz85n6jvcvj0nmwy3hpe739034rtssyaq8w";
+pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1ntfwhar55xe8hpfyg9g6suuvyg7223xy07ps692g8t6y866v69fsycdeka";
