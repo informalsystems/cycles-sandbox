58005fe17d43a61f2b1e712145a1f7382f1bbfa0
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-01-29 00:40:59 +0400

    Update OutputProof params

--- zk/arkworks/gramine/gen/output_id.rs
+++ zk/arkworks/gramine/gen/output_id.rs
@@ -1,3 +1,3 @@
 
-pub const PROVING_KEY_ID: &'static str = "groth16pk1s42lyta0al7pdtku4jjn7fmzsz06hsped7yrum830sx0gr7qyqzqdljlq0";
-pub const VERIFICATION_KEY_ID: &'static str = "groth16vk15wfgrm2yf8pfqfdqv07dl0z0ttpa2urhyvd862n2xvmscvyq256sr9fmhk";
+pub const PROVING_KEY_ID: &'static str = "groth16pk1ytpyw288wl2lq8kr8tdhwa2rvmtxpsczumuf86dv7gs69x6alras7k0374";
+pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1v34p9yv6dytxef8wmyxded90rdf64zgm68w5zf2k0gj4annw7fnq7pwlgn";
