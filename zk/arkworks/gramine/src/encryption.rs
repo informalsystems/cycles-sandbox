da6626e99e68f15f76dce408bc4747a75202f0d7
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-01-29 23:17:39 +0400

    Rename vars and fix API

--- zk/arkworks/gramine/src/encryption.rs
+++ zk/arkworks/gramine/src/encryption.rs
@@ -6,49 +6,44 @@ use poseidon377::hash_2;
 pub(crate) static ENC_DOMAIN_SEP: Lazy<Fq> =
     Lazy::new(|| Fq::from_le_bytes_mod_order(b"CyclesEncryption"));
 
-pub fn ecies_encrypt(
-    sender_sk: &Secret,
-    receiver_pk: Public,
-    msg: Vec<Fq>,
-) -> Result<Vec<Fq>, Error> {
-    let shared_secret = Encoding(sender_sk.key_agreement_with(&receiver_pk)?.0)
+pub fn ecies_encrypt(pk: Public, r: &Secret, msg: Vec<Fq>) -> Result<(Public, Vec<Fq>), Error> {
+    // compute s = r * pk
+    let s = Encoding(r.key_agreement_with(&pk)?.0)
         .vartime_decompress()
-        .map_err(|_| Error::InvalidPublic(receiver_pk))?;
+        .map_err(|_| Error::InvalidPublic(pk))?;
 
-    let mut ciphertext = vec![];
+    // compute c1 = r * generator
+    let c1 = r.public();
+
+    let mut c2 = vec![];
     for (i, fq) in msg.into_iter().enumerate() {
-        let hash_key = hash_2(
+        let h = hash_2(
             &ENC_DOMAIN_SEP,
-            (
-                shared_secret.vartime_compress_to_field(),
-                Fq::from(i as u128),
-            ),
+            (s.vartime_compress_to_field(), Fq::from(i as u128)),
         );
-        ciphertext.push(fq + hash_key);
+
+        // compute c2 = m + s
+        c2.push(fq + h);
     }
 
-    Ok(ciphertext)
+    Ok((c1, c2))
 }
 
-pub fn ecies_decrypt(
-    receiver_sk: &Secret,
-    sender_pk: Public,
-    ciphertext: Vec<Fq>,
-) -> Result<Vec<Fq>, Error> {
-    let shared_secret = Encoding(receiver_sk.key_agreement_with(&sender_pk)?.0)
+pub fn ecies_decrypt(sk: &Secret, (c1, c2): (Public, Vec<Fq>)) -> Result<Vec<Fq>, Error> {
+    // compute s = sk * c1
+    let s = Encoding(sk.key_agreement_with(&c1)?.0)
         .vartime_decompress()
-        .map_err(|_| Error::InvalidPublic(sender_pk))?;
+        .map_err(|_| Error::InvalidPublic(c1))?;
 
     let mut msg = vec![];
-    for (i, fq) in ciphertext.into_iter().enumerate() {
-        let hash_key = hash_2(
+    for (i, fq) in c2.into_iter().enumerate() {
+        let h = hash_2(
             &ENC_DOMAIN_SEP,
-            (
-                shared_secret.vartime_compress_to_field(),
-                Fq::from(i as u128),
-            ),
+            (s.vartime_compress_to_field(), Fq::from(i as u128)),
         );
-        msg.push(fq - hash_key);
+
+        // compute message = c2 - s
+        msg.push(fq - h);
     }
 
     Ok(msg)
@@ -65,12 +60,14 @@ mod tests {
     #[test]
     fn test_encryption_roundtrip() {
         let mut rng = OsRng;
-        let sender_sk = Secret::new(&mut rng);
+        let r = Secret::new(&mut rng);
         let receiver_sk = Secret::new(&mut rng);
 
         let msg: Vec<_> = [0..32].into_iter().map(|_| Fq::rand(&mut rng)).collect();
-        let ciphertext = ecies_encrypt(&sender_sk, receiver_sk.public(), msg.clone()).unwrap();
-        let msg_dec = ecies_decrypt(&receiver_sk, sender_sk.public(), ciphertext).unwrap();
+        let (g_r, ciphertext) = ecies_encrypt(receiver_sk.public(), &r, msg.clone()).unwrap();
+        let msg_dec = ecies_decrypt(&receiver_sk, (g_r, ciphertext)).unwrap();
         assert_eq!(msg, msg_dec);
     }
 }
+
+pub mod r1cs {}
