9de101ec0ab2a8f1dd1474ca5d50b6bf2a279628
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-01-29 19:04:51 +0400

    Polish API

--- zk/arkworks/gramine/src/encryption.rs
+++ zk/arkworks/gramine/src/encryption.rs
@@ -7,7 +7,7 @@ pub(crate) static ENC_DOMAIN_SEP: Lazy<Fq> =
     Lazy::new(|| Fq::from_le_bytes_mod_order(b"CyclesEncryption"));
 
 pub fn ecies_encrypt(
-    sender_sk: Secret,
+    sender_sk: &Secret,
     receiver_pk: Public,
     msg: Vec<Fq>,
 ) -> Result<Vec<Fq>, Error> {
@@ -31,7 +31,7 @@ pub fn ecies_encrypt(
 }
 
 pub fn ecies_decrypt(
-    receiver_sk: Secret,
+    receiver_sk: &Secret,
     sender_pk: Public,
     ciphertext: Vec<Fq>,
 ) -> Result<Vec<Fq>, Error> {
@@ -69,9 +69,8 @@ mod tests {
         let receiver_sk = Secret::new(&mut rng);
 
         let msg: Vec<_> = [0..32].into_iter().map(|_| Fq::rand(&mut rng)).collect();
-        let (sender_pk, ciphertext) =
-            ecies_encrypt(sender_sk, receiver_sk.public(), msg.clone()).unwrap();
-        let msg_dec = ecies_decrypt(receiver_sk, sender_pk, ciphertext).unwrap();
+        let ciphertext = ecies_encrypt(&sender_sk, receiver_sk.public(), msg.clone()).unwrap();
+        let msg_dec = ecies_decrypt(&receiver_sk, sender_sk.public(), ciphertext).unwrap();
         assert_eq!(msg, msg_dec);
     }
 }
