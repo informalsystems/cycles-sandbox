7217b60cbc425cf583740a1f8095751b1002d7b8
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-02-01 00:24:18 +0400

    Test ecies_decrypt circuit

--- zk/arkworks/gramine/src/encryption/r1cs.rs
+++ zk/arkworks/gramine/src/encryption/r1cs.rs
@@ -16,8 +16,17 @@ impl AllocVar<Plaintext, Fq> for PlaintextVar {
         f: impl FnOnce() -> Result<T, SynthesisError>,
         mode: AllocationMode,
     ) -> Result<Self, SynthesisError> {
+        let ns = cs.into();
+        let cs = ns.cs();
         let plaintext = Vec::<FqVar>::new_variable(cs, || Ok(f()?.borrow().clone()), mode)?;
         Ok(Self(plaintext))
+    }
+}
+
+impl EqGadget<Fq> for PlaintextVar {
+    #[inline]
+    fn is_eq(&self, other: &Self) -> Result<Boolean<Fq>, SynthesisError> {
+        Ok(self.0.is_eq(&other.0)?)
     }
 }
 
@@ -30,6 +39,8 @@ impl AllocVar<SharedSecret, Fq> for SharedSecretVar {
         f: impl FnOnce() -> Result<T, SynthesisError>,
         mode: AllocationMode,
     ) -> Result<Self, SynthesisError> {
+        let ns = cs.into();
+        let cs = ns.cs();
         let pk = ElementVar::new_variable(cs, || Ok(*f()?.borrow()), mode)?;
         Ok(Self(pk))
     }
@@ -138,23 +149,58 @@ mod test {
         // construct constraint system
         let cs = ConstraintSystem::<Fq>::new_ref();
         let m_var =
-            PlaintextVar::new_witness(ark_relations::ns!(cs, "gadget_message"), || Ok(&m)).unwrap();
+            PlaintextVar::new_witness(ark_relations::ns!(cs, "gadget_m"), || Ok(&m)).unwrap();
         let s_var =
-            SharedSecretVar::new_witness(ark_relations::ns!(cs, "gadget_public_key"), || {
-                Ok(&s_element)
-            })
-            .unwrap();
+            SharedSecretVar::new_witness(ark_relations::ns!(cs, "gadget_s"), || Ok(&s_element))
+                .unwrap();
 
         // use gadget
         let c2_var = super::ecies_encrypt(&s_var, &m_var).unwrap();
 
         // check that result equals expected ciphertext in the constraint system
         let expected_c2_var =
-            CiphertextVar::new_input(ark_relations::ns!(cs, "gadget_expected"), || Ok(&c2))
+            CiphertextVar::new_input(ark_relations::ns!(cs, "gadget_expected_c2"), || Ok(&c2))
                 .unwrap();
         expected_c2_var.enforce_equal(&c2_var).unwrap();
 
         assert_eq!(c2, c2_var.0.value().unwrap());
+        assert!(cs.is_satisfied().unwrap());
+    }
+
+    #[test]
+    fn test_ecies_decrypt() {
+        let rng = &mut OsRng;
+
+        // compute primitive result
+        let s = {
+            let rseed = Rseed::generate(&mut OsRng);
+            let esk = rseed.derive_esk();
+            let addr = test_keys::ADDRESS_1.clone();
+            let pkd = addr.transmission_key();
+            esk.key_agreement_with(pkd).unwrap()
+        };
+        let s_element = Encoding(s.0).vartime_decompress().unwrap();
+        let m: Vec<_> = [0..32].into_iter().map(|_| Fq::rand(rng)).collect();
+        let c2 = ecies_encrypt(s_element, m.clone()).unwrap();
+
+        // construct constraint system
+        let cs = ConstraintSystem::<Fq>::new_ref();
+        let c2_var =
+            CiphertextVar::new_input(ark_relations::ns!(cs, "gadget_c2"), || Ok(&c2)).unwrap();
+        let s_var =
+            SharedSecretVar::new_witness(ark_relations::ns!(cs, "gadget_s"), || Ok(&s_element))
+                .unwrap();
+
+        // use gadget
+        let m_var = super::ecies_decrypt(&s_var, &c2_var).unwrap();
+
+        // check that result equals expected ciphertext in the constraint system
+        let expected_m_var =
+            PlaintextVar::new_input(ark_relations::ns!(cs, "gadget_expected_m"), || Ok(&m))
+                .unwrap();
+        expected_m_var.enforce_equal(&m_var).unwrap();
+
+        assert_eq!(m, m_var.0.value().unwrap());
         assert!(cs.is_satisfied().unwrap());
     }
 }
