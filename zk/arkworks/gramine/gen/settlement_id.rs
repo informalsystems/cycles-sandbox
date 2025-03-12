1c088d443f9550105600a0877dcef3087398ba5b
Shoaib Ahmed <sufialhussaini@gmail.com>
2025-03-13 02:35:40 +0400

    fix: sub overflow bug in settlement circuit (#28)
    * Only use non-zero notes for calculating expected_output_note_commitment_vars
    
    * Update pk,vks

--- zk/arkworks/gramine/gen/settlement_id.rs
+++ zk/arkworks/gramine/gen/settlement_id.rs
@@ -1,3 +1,3 @@
 
-pub const PROVING_KEY_ID: &'static str = "groth16pk1dvj9qqfw4xpkqkn2yrruyqj8jysggxr5zkh58herzug55d8mvgyqfyheac";
-pub const VERIFICATION_KEY_ID: &'static str = "groth16vk13aq8xtnn8wgsu4rdsmedjqshctky75cweakwhcdlhhjw4h7w9veqzqzmg8";
+pub const PROVING_KEY_ID: &'static str = "groth16pk1h9gkwz7cuzwfm6fsh6t688u2gwk7ga6rsx3tffcdk7pwcwhayzysmh62sp";
+pub const VERIFICATION_KEY_ID: &'static str = "groth16vk17gnydy5wejtgrlp39axuq4ms7nuxwd36k8m79q08naa8x9q5f3eq4sfke5";
